d507238cbbd5bced34fec81ed78f17d8952f61a1
Paul Borbon <paulborbon@yahoo.com>
2026-09-19 17:33:10 +0800

    Connect referral form to email service

--- javascript/config.js
+++ javascript/config.js
@@ -1,6 +1,6 @@
 window.PB_CONFIG = Object.freeze({
   version: "Beta 2.0",
-  referralBackendUrl: "", // Public URL of the new referral backend; configure after deployment.
+  referralBackendUrl: "https://promptbuilder-email.paulborbon.workers.dev", // Public URL of the new referral backend; configure after deployment.
   backendUrl: "https://promptbuilder-backend.paulborbon.workers.dev",
   createdDate: "13 Sep 2026",
   updatedDate: "19 Sep 2026",
